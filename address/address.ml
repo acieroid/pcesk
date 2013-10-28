14d442d174c34b474d6041a490c0dc9570ecda7c
Quentin Stievenart <quentin.stievenart@gmail.com>
2013-10-28 22:46:20 +0100

    Improve address module

--- address/address.ml
+++ address/address.ml
@@ -2,7 +2,30 @@ module type ADDRESS =
 sig
   type t
   val alloc : int -> t
+  val alloc_kont : int  -> t
   val compare : t -> t -> int
   val string_of_address : t -> string
 end
 
+module Mono_addr : ADDRESS =
+struct
+
+  type t =
+    | ValAddr of int
+    | KontAddr of int
+
+  let alloc tag = ValAddr tag
+
+  let alloc_kont tag = KontAddr tag
+
+  let compare x y = match x, y with
+    | ValAddr a, ValAddr b
+    | KontAddr a, KontAddr b -> Pervasives.compare a b
+    | ValAddr _, KontAddr _ -> 1
+    | KontAddr _, ValAddr _ -> -1
+
+  let string_of_address = function
+    | ValAddr a -> "val" ^ (string_of_int a)
+    | KontAddr a -> "kont" ^ (string_of_int a)
+
+end
