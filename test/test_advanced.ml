a146c4aeca3e420146c9952405e1d51b73e1dbe3
Quentin Stievenart <quentin.stievenart@gmail.com>
2013-11-10 14:50:37 +0100

    Fix advanced tests

--- test/test_advanced.ml
+++ test/test_advanced.ml
@@ -24,36 +24,29 @@ let test_match string expected cmp =
     (Lattice.abst1 expected) merged
 
 let test_multiple_calls () =
-  "(begin
-     (define sq (lambda (x)
-       (* x x)))
+  "(letrec ((sq (lambda (x) (* x x))))
      (sq 2)
      (sq 3))" => AbsInteger;
-  "(begin
-     (define inc (lambda (x)
-       (+ x 1)))
-      (inc (inc 2)))" => AbsInteger
+  "(letrec ((inc (lambda (x) (+ x 1))))
+     (inc (inc 2)))" => AbsInteger
 
 let test_recursive_calls () =
-  "(begin
-     (define count (lambda (n)
-       (if (= n 0)
-         \"done\"
-         (count (- n 1)))))
+  "(letrec ((count (lambda (n)
+                     (if (= n 0)
+                       \"done\"
+                       (count (- n 1))))))
      (count 200))" => AbsString;
-  "(begin
-     (define fact (lambda (n)
-       (if (= n 0)
-         1
-         (* n (fact (- n 1))))))
+  "(letrec ((fact (lambda (n)
+                    (if (= n 0)
+                      1
+                      (* n (fact (- n 1)))))))
      (fact 5))" => AbsInteger
 
 let test_fibo () =
-  "(begin
-     (define fib (lambda (n)
-       (if (< n 2)
-         n
-         (+ (fib (- n 1)) (fib (- n 2))))))
+  "(letrec ((fib (lambda (n)
+                   (if (< n 2)
+                     n
+                     (+ (fib (- n 1)) (fib (- n 2)))))))
      (fib 4))" => AbsInteger
 
 let test_widen () =
@@ -61,29 +54,24 @@ let test_widen () =
      this example will keep running, with values staying at the same
      "level" of the lattice, but with different values (in case the
      lattice as an infinite width) *)
-  "(begin
-     (define g (lambda ()
-       1))
-     (define f (lambda (n)
-       (if (= n 0)
-         0
-         (+ (f (- n 1)) (g)))))
+  "(letrec ((g (lambda ()
+                 1))
+            (f (lambda (n)
+                 (if (= n 0)
+                   0
+                   (+ (f (- n 1)) (g))))))
      (f 10))" => AbsInteger
 
 let test_church_numerals () =
   let church x =
-    "(begin
-  (define zero
-    (lambda (f x)
-      x))
-    (define inc
-      (lambda (n)
-        (lambda (f x)
-          (f (n f x)))))
-    (define plus
-      (lambda (m n)
-        (lambda (f x)
-          (m f (n f x)))))" ^ x ^ ")" in
+    "(letrec ((zero (lambda (f x) x))
+              (inc (lambda (n)
+                     (lambda (f x)
+                       (f (n f x)))))
+              (plus (lambda (m n)
+                      (lambda (f x)
+                        (m f (n f x))))))
+       " ^ x ^ ")" in
   let test_clo s =
     test_match (church s)
       (AbsUnique (Closure (([], []), Cesk_base.empty_env)))
@@ -106,8 +94,8 @@ let test_infinite () =
           "[" ^ (String.concat ", "
                    (List.map string_of_value l)) ^ "]")
       [] results in
-  test_no_result "(begin (define f (lambda () (f))) (f))";
-  test_no_result "(begin (define t (lambda (x) (t (+ x 1)))) (t 0))"
+  test_no_result "(letrec ((f (lambda () (f)))) (f))";
+  test_no_result "(letrec ((t (lambda (x) (t (+ x 1))))) (t 0))"
 
 let suite =
   "Advanced tests" >:::
