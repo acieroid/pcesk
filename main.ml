d5a82bfef5d5696fc0f37786fea22ccf72af1a19
Quentin Stievenart <quentin.stievenart@gmail.com>
2013-11-14 13:01:41 +0100

    Measure time and print number of nodes & edges

--- main.ml
+++ main.ml
@@ -1,6 +1,13 @@
 open Types
 open Params
 
+let now () = Unix.gettimeofday ()
+
+let print_infos time graph =
+  print_string "Time took: "; print_float time; print_newline ();
+  print_string "Nodes: "; print_int (Viz.G.nb_vertex graph); print_newline ();
+  print_string "Edges: "; print_int (Viz.G.nb_edges graph); print_newline ()
+
 let () =
   Arg.parse speclist
     (fun x -> raise (Arg.Bad ("Bad argument : " ^ x)))
@@ -9,7 +16,9 @@ let () =
     print_string ("Running with: \n" ^ (string_of_configuration ()));
     print_newline ();
     let node = Parser.parse (Lexer.lex !input) in
+    let start = now () in
     let res, graph = Cesk.eval node in
+    let stop = now () in
     List.iter (fun (value, env, store) ->
         print_string (string_of_value value); print_newline ())
       res;
@@ -17,6 +26,7 @@ let () =
       | Some f -> Viz.output_graph f graph
       | None -> ()
     end;
-    close_in !input
+    close_in !input;
+    print_infos (stop -. start) graph
   with
   | e -> print_string (Exceptions.string_of_exception e)
