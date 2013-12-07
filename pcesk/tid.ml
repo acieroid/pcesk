cc401c542576d11af44f6d333915ead7ea7e1810
Quentin Stievenart <quentin.stievenart@gmail.com>
2013-12-07 15:57:48 +0100

    Add abstract tid with context bound and k last call sites

--- pcesk/tid.ml
+++ pcesk/tid.ml
@@ -1,5 +1,6 @@
 open Types
 open Pcesk_types
+open Cesk_types
 
 module type TID = sig
   val newtid : int -> context -> threads -> tid
@@ -7,4 +8,15 @@ end
 
 module ConcreteTID = struct
   let newtid n _ _ = IntTid (n+1)
+end
+
+module ContextBoundTID =
+  functor (Size : sig val size : int end) -> struct
+    let newtid n _ _ = IntTid (n mod Size.size)
+  end
+
+module TimestampTid = struct
+  let newtid _ context _ = match context.cexp with
+    | Node (_, tag) -> TagTid (tag, context.ctime)
+    | _ -> failwith "should not happen"
 end
