dabbceb24ce0b093e8cf453f82cc96f4ac4ba8fc
Quentin Stievenart <quentin.stievenart@gmail.com>
2013-12-29 14:45:32 +0100

    Move sequential benchmarks

--- bench/run.sh
+++ bench/run.sh
@@ -1,7 +1,7 @@
 TIMEOUT=30
 FAIL=124
 CMD="timeout $TIMEOUT ./main.byte -quiet"
-FILES=$(find input/ -type f -name \*.scm)
+FILES=$(find input/bench-seq -type f -name \*.scm)
 
 function run {
   FILE="$1"
@@ -14,12 +14,17 @@ function run {
   fi
 }
 
-printf "%25.25s\t%s\t\t%s\t\t%s\t\t%s\n" "file" "0" "0+gc" "1" "1+gc"
+printf "%25.25s\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\n" "file" "0" "0+gc" "0+s" "0+s+gc" "1" "1+gc" "1+s" "1+s+gc"
 for f in $FILES; do
-  printf "%25.25s\t" $f
+  basef=$(basename $f)
+  printf "%25.25s\t" $basef
   run $f -k 0
-  run $f -k 0 -gc
+  run $f -k 0 -gc-after
+  run $f -k 0 -s
+  run $f -k 0 -gc-after -s
   run $f -k 1
-  run $f -k 1 -gc
+  run $f -k 1 -gc-after
+  run $f -k 1 -s
+  run $f -k 1 -gc-after -s
   echo ""
 done
