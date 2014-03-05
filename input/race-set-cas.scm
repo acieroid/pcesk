e4b76854ef3255bc90644752aa3833f04559e849
Quentin Stievenart <quentin.stievenart@gmail.com>
2014-03-05 11:35:31 +0100

    Introduce a *real* race condition in race-set-cas

--- input/race-set-cas.scm
+++ input/race-set-cas.scm
@@ -1,6 +1,6 @@
 (letrec ((x 0)
-         (t1 (spawn (cas x 0 1)))
-         (t2 (spawn (set! x 2))))
+         (t1 (spawn (set! x 2)))
+         (t2 (spawn (cas x 2 1))))
   (join t1)
   (join t2)
   x)
