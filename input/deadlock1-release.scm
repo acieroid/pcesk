32b399480b2e15b3a35369fa31db7ca344bdf3ea
Quentin Stievenart <quentin.stievenart@gmail.com>
2014-04-02 22:40:20 +0200

    Add another deadlock example

---
+++ input/deadlock1-release.scm
@@ -0,0 +1,15 @@
+;; Deadlock involving only one lock, but two threads
+(letrec ((lock #f)
+         (acquire (lambda ()
+                    (if (cas lock #f #t)
+                        nil
+                        (acquire))))
+         (release (lambda ()
+                      (set! lock #f)))
+         (t1 (spawn (begin
+                      (acquire)
+                      ; forgot to release the lock!
+                      ))))
+  (acquire)
+  (release)
+  (join t1))
