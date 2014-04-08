68452f2029fa6093ab2d3f329a7c721860ad41ac
Quentin Stievenart <quentin.stievenart@gmail.com>
2014-04-08 10:26:11 +0200

    Add script for locks benchmarks

---
+++ bench/locks.sh
@@ -0,0 +1,31 @@
+TIMEOUT=300 # 5 min
+FAIL=124
+CMD="timeout $TIMEOUT ./main.byte -quiet -l -p -r -j"
+FILES=$(find input/locks -type f -name \*.scm)
+
+function run {
+  FILE="$1"
+  shift
+  RES=$($CMD -i "$FILE" "$@")
+  if [ "$?" -eq "$FAIL" ]; then
+    echo -n -e "---\t"
+  else
+    echo -n -e $RES "\t"
+  fi
+}
+
+printf "%25.25s\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\n" "file" "nothing" "gc" "sbfs" "sdfs" "gc+sbfs" "gc+sdfs"
+for k in $(seq 0 2); do
+  echo "k = $k -------------------------------------------------"
+  for f in $FILES; do
+    basef=$(basename $f)
+    printf "%25.25s\t" $basef
+    run $f -k $k
+    run $f -k $k -gc
+    run $f -k $k -s -e bfs
+    run $f -k $k -s -e dfs
+    run $f -k $k -gc -s -e bfs
+    run $f -k $k -gc -s -e dfs
+    echo
+  done
+done
