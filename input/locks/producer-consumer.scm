8da847a350e6b74b1cea12f560a24bbb279fc5c3
Quentin Stievenart <quentin.stievenart@gmail.com>
2014-04-08 10:19:12 +0200

    Add more lock examples

---
+++ input/locks/producer-consumer.scm
@@ -0,0 +1,31 @@
+(letrec ((buffer nil)
+         (done #f)
+         (lock #unlocked)
+         (do-something (lambda (element)
+                         ;; Function called on the consumed elements
+                         nil))
+         (producer (lambda (n)
+                     ;; Produce numbers from n to 0, storing them in buffer
+                     (if (= n 0)
+                         (set! done #t) ; will only work with one producer
+                         (begin
+                           (acquire lock)
+                           (set! buffer (cons n buffer))
+                           (release lock)
+                           (producer (- n 1))))))
+         (consumer (lambda ()
+                     ;; Consume an element of buffer until done is #t
+                     (if done
+                         nil
+                         (if (empty? buffer)
+                             (consumer)
+                             (begin
+                               (acquire lock)
+                               (do-something (car buffer))
+                               (set! buffer (cdr buffer))
+                               (release lock)
+                               (consumer))))))
+         (producer-thread (spawn (producer 5)))
+         (consumer-thread (spawn (consumer))))
+  (join producer-thread)
+  (join consumer-thread))
